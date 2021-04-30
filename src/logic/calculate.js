8eeb3c62c6a8a44c4eac578eb23cb33247369fef
Yanick <tyankee15@gmail.com>
2021-04-30 12:45:01 -0300

    implement logic for first case of calculate's operations

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,7 +1,20 @@
-import operate from './Operate';
+import Operate from './Operate';
 
-function Calculate(dataObj, buttonNmae) {
-  let { total, next operation } = dataObj;
+function Calculate(dataObj, buttonName) {
+  let { total, next, operation } = dataObj;
+
+  switch (buttonName) {
+    case '%':
+      if (next) {
+        total = Operate(total, next, operation);
+        total = Operate(total, '100', '÷');
+        next = null;
+        operation = null;
+      } else {
+        total = Operate(total, '100', '÷')
+      }
+      break;
+  }
 }
 
 export default Calculate;
