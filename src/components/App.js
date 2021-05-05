8b145fe5a54de6b7fcd766f70498325aebc3f06c
Yanick <tyankee15@gmail.com>
2021-05-05 19:28:41 -0300

    Fix handleClick and redrer in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,33 @@ import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import Calculate from '../logic/Calculate';
 
+/* eslint-disable react/destructuring-assignment, react/no-access-state-in-setstate */
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      total: '',
-      next: '',
-      operation: '',
+      total: null,
+      next: null,
+      operation: null,
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // handleClick(buttonName) {
+  //   const { total, next, operation } = Calculate(this.state, buttonName);
+  //   this.setState({ total, next, operation });
+  // }
+
   handleClick(buttonName) {
-    const { total, next, operation } = Calculate(this.state, buttonName);
-    this.setState({ total, next, operation });
+    this.setState(Calculate(this.state, buttonName));
   }
 
   render() {
-    const { total, next, operation } = this.state;
+    // const { total, next, operation } = this.state;
     return (
       <div className="App">
-        <Display result={String(total)} next={String(next)} operation={(operation)} />
+        <Display result={this.state.next || this.state.total || '0'} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
