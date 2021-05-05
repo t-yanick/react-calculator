b65df74c7073a7b4de7cb6e21645bf2bb31e0153
Yanick <tyankee15@gmail.com>
2021-05-05 13:42:54 -0300

    Implement handleClick function and pass clickHandler property to ButtonPanel

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-// import Calculate from '../logic/Calculate';
+import Calculate from '../logic/Calculate';
 
-function App() {
-  return (
-    <React.Fragment className="App">
-      <Display />
-      <ButtonPanel />
-    </React.Fragment>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = Calculate(this.state, buttonName);
+    this.setState({ total, next, operation });
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div className="App">
+        <Display result={String(total)} next={String(next)} operation={(operation)} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
-
-export default App;
